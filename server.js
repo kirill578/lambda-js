c9a83b0d6a08a91bc20103555e02103b00ad9e55
Kirill Kulakov <kirill578@gmail.com>
2019-10-18 20:27:39 -0500

    cors

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ const saveDb = (id, db) => {
 };
 
 const asyncMiddleware = fn => (req, res, next) => {
+    res.set({'Access-Control-Allow-Origin': '*'});
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
